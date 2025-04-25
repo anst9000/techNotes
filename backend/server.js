70dace9661dc9009c5ff499fa2c305914c0f55cf
Anders Strömberg <coltlaboy@yahoo.se>
2025-04-25 10:31:26 +0200

    Connection to MongoDB clear

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
+require("dotenv").config()
 const express = require("express")
 const path = require("path")
-const { logger } = require("./middleware/logger")
+const { logger, logEvents } = require("./middleware/logger")
 const errorHandler = require("./middleware/errorHandler")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
 const corsOptions = require("./config/corsOptions")
+const connectDB = require("./config/dbConn")
+const mongoose = require("mongoose")
 
 const PORT = process.env.PORT || 3500
+
+console.log("--> Environment is", process.env.NODE_ENV)
+connectDB()
 
 const app = express()
 
@@ -31,6 +37,18 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler)
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+mongoose.connection.once("open", () => {
+  console.log("--> Connected to MongoDB")
+
+  app.listen(PORT, () => {
+    console.log(`--> Server running on port ${PORT}`)
+  })
+})
+
+mongoose.connection.on("error", (err) => {
+  console.log(err)
+  logEvents(
+    `${err.ok}: ${err.errmsg}\t${err.code}\t${err.codeName}`,
+    "mongoErrLog.log"
+  )
 })
